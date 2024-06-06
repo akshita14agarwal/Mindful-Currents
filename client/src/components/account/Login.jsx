c2358149456bfdd18fc52150d26721ea09e00c0a
Ashna Purwar <ashna.20224038@mnnit.ac.in>
2024-06-06 14:21:11 +0530

    day2

--- client/src/components/account/Login.jsx
+++ client/src/components/account/Login.jsx
@@ -2,6 +2,8 @@ import{useState} from 'react';
 
 import {Box, TextField, Button, styled, Typography} from '@mui/material';
 
+import {API} from '../../service/api';
+
 const Component = styled(Box)`
 width: 400px;
 margin:auto;
@@ -39,18 +41,47 @@ const SignupButton = styled(Button)`
     border-radius: 2px;
     box-shadow: 0 2px 4px 0 rgb(0 0 0 / 20%);
 `;
+
+const Error = styled(Typography)`
+    font-size: 10px;
+    color: #ff6161;
+    line-height: 0;
+    margin-top: 10px;
+    font-weight: 600;
+`
 const Text = styled(Typography)`
     color: #878787;
     font-size: 16px;
 `;
+const signupInitialValues = {
+    name: '',
+    username: '',
+    password: '',
+};
 
 const Login = () => {
 
     const imageURL = 'https://www.sesta.it/wp-content/uploads/2021/03/logo-blog-sesta-trasparente.png';
 
     const[account, toggleAccount]= useState('login');
+    const[signup, setSignup]=useState(signupInitialValues);
+    const[error,setError]=useState('');
     const toggleSignup = () =>{
         account === 'signup' ? toggleAccount('login'): toggleAccount('signup');
+    }
+    const onInputChange = (e) => {
+        setSignup({ ...signup, [e.target.name]: e.target.value });
+    }
+    const signupUser = async() => {
+        let response =await API.userSignup(signup);
+        if(response.isSuccess){
+            setError('');
+            setSignup(signupInitialValues);
+            toggleAccount('login')
+        }else{
+            setError('Something went wrong.PLease try again later');
+
+        }
     }
     return(
         <Component>
@@ -61,16 +92,21 @@ const Login = () => {
                         <Wrapper>
                             <TextField variant="standard" label="Enter username"/>
                             <TextField variant="standard" label="Enter password"/>
+
+                            {error && <Error>{error}</Error>}
+
                             <LoginButton variant="contained">Login</LoginButton>
                             <Text style={{textAlign: 'center'}}>OR</Text>
                             <SignupButton onClick={()=> toggleSignup()}>Create an account</SignupButton>
                         </Wrapper>
                     :
                         <Wrapper>
-                            <TextField variant="standard" label="Enter Name"/>
-                            <TextField variant="standard" label="Enter Username"/>
-                            <TextField variant="standard" label="Enter Password"/>
-                            <SignupButton>Signup</SignupButton>
+                            <TextField variant="standard" onChange={(e) => onInputChange(e)} name='name' label='Enter Name' />
+                            <TextField variant="standard" onChange={(e) => onInputChange(e)} name='username' label='Enter Username' />
+                            <TextField variant="standard" onChange={(e) => onInputChange(e)} name='password' label='Enter Password' />
+
+                            {error && <Error>{error}</Error>}
+                            <SignupButton onClick={() => signupUser}>Signup</SignupButton>
                             <Text style={{textAlign: 'center'}}>OR</Text>
                             <LoginButton variant="contained" onClick={()=> toggleSignup()}>Already have an account</LoginButton>
                         </Wrapper>
