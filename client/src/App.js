e1ba846cebaf1044bbf07f6ab78aac460c70ea3a
drxti <driti2610@gmail.com>
2024-06-25 11:40:59 +0530

    post page changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,53 +1,66 @@
-import React from 'react';
 import { useState } from 'react';
-import DataProvider from './context/DataProvider';
 
-import {BrowserRouter, Routes, Route, Outlet, Navigate} from 'react-router-dom';
-
+import { Box } from '@mui/material';
+import { BrowserRouter, Routes, Route, Navigate, Outlet } from 'react-router-dom';
 
 //components
-import Login from './components/account/Login';
-import Home from './components/home/Home';
+import DataProvider from './context/DataProvider';
 import Header from './components/header/Header';
+import Home from './components/home/Home';
 import CreatePost from './components/create/CreatePost';
+import DetailView from './components/details/DetailView';
+import Update from './components/create/Update';
+import About from './components/about/About';
+import Contact from './components/contact/Contact';
+import Login from './components/account/Login';
 
-const PrivateRoute =({isAuthenticated, ...props}) => {
-
-
-return isAuthenticated ?
-<>
-
-<Header/>
-
-<Outlet/>
-</>
-
-: <Navigate replace to='/login'/>
-}
+const PrivateRoute = ({ isAuthenticated, ...props }) => {
+  const token = sessionStorage.getItem('accessToken');
+  return isAuthenticated && token ? 
+    <>
+      <Header />
+      <Outlet />
+    </> : <Navigate replace to='/account' />
+};
 
 function App() {
 
-  const [isAuthenticated, isUserAuthenticated]= useState(false);
+  const [isAuthenticated, isUserAuthenticated] = useState(false);
+
   return (
-  <DataProvider>
-    <BrowserRouter>
-        <div style={{marginTop: 64}}>
+    <DataProvider>
+      <BrowserRouter>
+        <Box style={{ marginTop: 64 }}>
           <Routes>
-            <Route path='/login' element ={<Login isUserAuthenticated ={isUserAuthenticated}/>}/>
-
-            <Route path ='/' element ={<PrivateRoute isAuthenticated={isAuthenticated}/>}>
-              <Route path='/' element = {<Home/>}/>
-            </Route>
-
+            <Route path='/account' element={<Login isUserAuthenticated={isUserAuthenticated} />} />
             
-            <Route path ='/create' element ={<PrivateRoute isAuthenticated={isAuthenticated}/>}>
-              <Route path='/create' element = {<CreatePost/>}/>
+            <Route path='/' element={<PrivateRoute isAuthenticated={isAuthenticated} />} >
+              <Route path='/' element={<Home />} />
             </Route>
 
-        </Routes>
-      </div>
-    </BrowserRouter>
-  </DataProvider>
+            <Route path='/create' element={<PrivateRoute isAuthenticated={isAuthenticated} />} >
+              <Route path='/create' element={<CreatePost />} />
+            </Route>
+
+            <Route path='/details/:id' element={<PrivateRoute isAuthenticated={isAuthenticated} />} >
+              <Route path='/details/:id' element={<DetailView />} />
+            </Route>
+
+            <Route path='/update/:id' element={<PrivateRoute isAuthenticated={isAuthenticated} />} >
+              <Route path='/update/:id' element={<Update />} />
+            </Route>
+
+            <Route path='/about' element={<PrivateRoute isAuthenticated={isAuthenticated} />} >
+              <Route path='/about' element={<About />} />
+            </Route>
+
+            <Route path='/contact' element={<PrivateRoute isAuthenticated={isAuthenticated} />} >
+              <Route path='/contact' element={<Contact />} />
+            </Route>
+          </Routes>
+        </Box>
+      </BrowserRouter>
+    </DataProvider>
   );
 }
 
--- client/src/service/api.js
+++ client/src/service/api.js
@@ -1,106 +1,142 @@
-
 import axios from 'axios';
-import {API_NOTIFICATION_MESSAGES, SERVICE_URLS} from '../constants/config';
 
-import { getAccessToken } from '../utils/common-utils';
-const API_URL=' http://localhost:8000';
-const axiosInstance=axios.create({
-    baseURL:API_URL,
-    timeout: 10000,
+import { API_NOTIFICATION_MESSAGES, SERVICE_URLS } from '../constants/config';
+import { getAccessToken, getRefreshToken, setAccessToken, getType } from '../utils/common-utils';
+
+const API_URL = 'http://localhost:8000';
+
+const axiosInstance = axios.create({
+    baseURL: API_URL,
+    timeout: 10000, 
     headers: {
         "content-type": "application/json"
     }
-})
+});
+
 axiosInstance.interceptors.request.use(
-    function (config){
-        return config; 
+    function(config) {
+        if (config.TYPE.params) {
+            config.params = config.TYPE.params
+        } else if (config.TYPE.query) {
+            config.url = config.url + '/' + config.TYPE.query;
+        }
+        return config;
     },
-    function (error){
+    function(error) {
         return Promise.reject(error);
     }
-)
+);
 
 axiosInstance.interceptors.response.use(
-    function (response){
-        //global loader stopped here
+    function(response) {
+        // Stop global loader here
         return processResponse(response);
     },
-
-    function(error){
-        //stop global leader here
-        return Promise.reject(processError(error)) ;
-
+    function(error) {
+        // Stop global loader here
+        return Promise.reject(ProcessError(error));
     }
 )
-//if success ---return {isSuccess: true,data: Object} 
-//if failure ---return {isFailure: true, status: string,msg: string, code: int}
-const processResponse=(response) =>{
-    if(response?.status ===200){
-        return{ isSuccess: true, data: response.data}
-    }else{
+
+///////////////////////////////
+// If success -> returns { isSuccess: true, data: object }
+// If fail -> returns { isFailure: true, status: string, msg: string, code: int }
+//////////////////////////////
+const processResponse = (response) => {
+    if (response?.status === 200) {
+        return { isSuccess: true, data: response.data }
+    } else {
         return {
             isFailure: true,
             status: response?.status,
             msg: response?.msg,
             code: response?.code
         }
-
     }
 }
-const processError = (error) => {
-    if(error.response){ // request sent successfully but servers response falls out of range 
-        console.log('Error in response: ',error.toJSON());
-        return{
-            isError: true,
-            msg: API_NOTIFICATION_MESSAGES.responseFailure,
-            code: error.response.status
+
+///////////////////////////////
+// If success -> returns { isSuccess: true, data: object }
+// If fail -> returns { isError: true, status: string, msg: string, code: int }
+//////////////////////////////
+const ProcessError = async (error) => {
+    if (error.response) {
+        // Request made and server responded with a status code 
+        // that falls out of the range of 2xx
+        if (error.response?.status === 403) {
+            // const { url, config } = error.response;
+            // console.log(error);
+            // try {
+            //     let response = await API.getRefreshToken({ token: getRefreshToken() });
+            //     if (response.isSuccess) {
+                    sessionStorage.clear();
+            //         setAccessToken(response.data.accessToken);
+
+            //         const requestData = error.toJSON();
+
+            //         let response1 = await axios({
+            //             method: requestData.config.method,
+            //             url: requestData.config.baseURL + requestData.config.url,
+            //             headers: { "content-type": "application/json", "authorization": getAccessToken() },
+            //             params: requestData.config.params
+            //         });
+            //     }
+            // } catch (error) {
+            //     return Promise.reject(error)
+            // }
+        } else {
+            console.log("ERROR IN RESPONSE: ", error.toJSON());
+            return {
+                isError: true,
+                msg: API_NOTIFICATION_MESSAGES.responseFailure,
+                code: error.response.status
+            }
         }
-    }else if(error.request){ //request sent but no response received
-        console.log('Error in request: ',error.toJSON());
-        return{
+    } else if (error.request) { 
+        // The request was made but no response was received
+        console.log("ERROR IN RESPONSE: ", error.toJSON());
+        return {
             isError: true,
             msg: API_NOTIFICATION_MESSAGES.requestFailure,
-            code: ""  //no req sent to backend
+            code: ""
         }
-
-
-    }else{ //error on the frontend
-        console.log('Error in network: ',error.toJSON());
-        return{
+    } else { 
+        // Something happened in setting up the request that triggered an Error
+        console.log("ERROR IN RESPONSE: ", error.toJSON());
+        return {
             isError: true,
-            msg: API_NOTIFICATION_MESSAGES.networkFailure,
-            code: "" //no req sent to backend
+            msg: API_NOTIFICATION_MESSAGES.networkError,
+            code: ""
         }
-
     }
 }
-const API= {};
 
-for(const [key,value] of Object.entries(SERVICE_URLS)){
-    API[key]=(body,showUploadProgress,showDownloadProgress) => // to show the progress loader bar
+const API = {};
+
+for (const [key, value] of Object.entries(SERVICE_URLS)) {
+    API[key] = (body, showUploadProgress, showDownloadProgress) =>
         axiosInstance({
             method: value.method,
             url: value.url,
-            data: body,
+            data: value.method === 'DELETE' ? '' : body,
             responseType: value.responseType,
-            headers:{
-                authorization: getAccessToken()
+            headers: {
+                authorization: getAccessToken(),
             },
-            onUploadProgress: function (progressEvent){
-                if(showUploadProgress){
-                    let percentageCompleted = Math.round((progressEvent.loaded * 100 )/ progressEvent.total)
-                    showUploadProgress(percentageCompleted);
+            TYPE: getType(value, body),
+            onUploadProgress: function(progressEvent) {
+                if (showUploadProgress) {
+                    let percentCompleted = Math.round((progressEvent.loaded * 100) / progressEvent.total);
+                    showUploadProgress(percentCompleted);
                 }
             },
-            onDownloadProgress: function (progressEvent){
-                if(showDownloadProgress){
-                    let percentageCompleted = Math.round((progressEvent.loaded * 100 )/ progressEvent.total)
-                    showDownloadProgress(percentageCompleted);
+            onDownloadProgress: function(progressEvent) {
+                if (showDownloadProgress) {
+                    let percentCompleted = Math.round((progressEvent.loaded * 100) / progressEvent.total);
+                    showDownloadProgress(percentCompleted);
                 }
             }
+        });
+}
 
-        })
-
-    }
-
-export {API};
+export { API };
